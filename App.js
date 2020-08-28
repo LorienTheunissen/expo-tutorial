9c557c154abf861a1ff1cec47c9098922c5213e5
Lorien Theunissen <Lorien@Loriens-MacBook-Pro.local>
2020-08-28 20:34:05 +0200

    Handling platform differences

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import logo from './assets/logo.png';
 import * as ImagePicker from 'expo-image-picker';
 import * as Sharing from 'expo-sharing';
+import uploadToAnonymousFilesAsync from 'anonymous-files';
 
 export default function App() {
 
@@ -24,12 +25,17 @@ export default function App() {
       return;
     }
 
-    setSelectedImage({ localUri: pickerResult.uri });
+    if (Platform.OS === 'web') {
+        let remoteUri = await uploadToAnonymousFilesAsync(pickerResult.uri);
+        setSelectedImage({ localUri: pickerResult.uri, remoteUri });
+      } else {
+        setSelectedImage({ localUri: pickerResult.uri, remoteUri: null });
+      }
   };
 
   let openShareDialogAsync = async () => {
     if (!(await Sharing.isAvailableAsync())) {
-      alert(`Uh oh, sharing isn't available on your platform`);
+      alert(`The image is available for sharing at: ${selectedImage.remoteUri}`);
       return;
     }
 
